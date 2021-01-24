c0e8a6d057498985bf9ae95ee83844557a2c3ab0
qyz-project <67222678+qyz-project@users.noreply.github.com>
2021-01-24 15:15:07 +0100

    update

---
+++ assets/js/28.408a92eb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{379:function(n,t,a){"use strict";a.r(t);var e={components:{Navbar:a(344).a}},s=a(42),o=Object(s.a)(e,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",{},[t("Navbar"),this._v(" "),t("Content")],1)}),[],!1,null,"78a7512a",null);t.default=o.exports}}]);
